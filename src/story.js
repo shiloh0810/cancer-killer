47776f3398c100dcdd8dd2c9562b199fd56f3621
shiloh0810 <shiloh20040810@gmail.com>
2018-01-05 21:10:56 +0800

    story

--- src/story.js
+++ src/story.js
@@ -1,10 +1,35 @@
-var typed5 = new Typed('#story-text', {
-    strings: ['1 Some <i>strings</i> with', '2 Some <strong>HTML</strong>', '3 Chars &times; &copy;'],
-    typeSpeed: 0,
-    backSpeed: 0,
-    cursorChar: '_',
-    shuffle: true,
-    smartBackspace: false,
-    loop: true
+// var typed5 = new Typed('#story-text', {
+//     strings: ['', 'Emergency! Emergency!'],
+// });
+
+function showStory_0(){
+
+}
+
+function showStory_1(){
+    $("#story").css("background-image", "url(./assets/hospital.png)")
+    var typed5 = new Typed('#story-text', {
+        strings: ['', 'Emergency! Emergency!'],
+    });
+}
+
+function showStory_2(){
+    $("#avatar").css("background-image", "url(./assets/doctor.svg)")
+    var typed5 = new Typed('#story-text', {
+        strings: ['', 'What happened?'],
+    });
+}
+
+$( "body" ).keydown(function( event ) {
+    if ( event.which == 13 ) {
+     nextStory();
+    }
   });
-    
+
+var current = 0;
+
+  function nextStory(){
+    if(current==0) showStory_1();
+    if(current==1) showStory_2();
+    current++;
+  }
