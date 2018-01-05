726debd1540ddece6f113ff2ef1c5d963e942594
shiloh0810 <shiloh20040810@gmail.com>
2018-01-05 20:32:15 +0800

    typed

---
+++ src/story.js
@@ -0,0 +1,10 @@
+var typed5 = new Typed('#story-text', {
+    strings: ['1 Some <i>strings</i> with', '2 Some <strong>HTML</strong>', '3 Chars &times; &copy;'],
+    typeSpeed: 0,
+    backSpeed: 0,
+    cursorChar: '_',
+    shuffle: true,
+    smartBackspace: false,
+    loop: true
+  });
+    
