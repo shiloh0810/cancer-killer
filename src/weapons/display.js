3fbb9c17b6150000cc9fb1ac9e8130da6dd9b965
shiloh0810 <shiloh20040810@gmail.com>
2018-01-14 22:31:26 +0800

    change display again

--- src/weapons/display.js
+++ src/weapons/display.js
@@ -4,10 +4,10 @@ function display() {
     delayShow('.js-weapons_1', 'cryo ablation', 0);
     delayShow('.js-weapons_2', 'surgery', 0);
     delayShow('.js-weapons_3', 'chemotherapy', 20000);
-    delayShow('.js-weapons_4', 'radiation therapy', 40000);
+    delayShow('.js-weapons_4', 'radiation therapy', 30000);
     // delayShow('.js-weapons_5', 5, 30000);
     // delayShow('.js-weapons_6', 6, 4000);
-    delayShow('.js-weapons_7', 'transplantation', 60000);
+    delayShow('.js-weapons_7', 'transplantation', 40000);
     //delayShow('.js-weapons_8', 'Immuno', 40000);
 
     function delayShow(element, key, time) {
